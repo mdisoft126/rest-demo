1bd51530971163d3e222d31138c2fd22d98e9e63
mdisoft126 <mdisoft126@gmail.com>
2021-11-24 21:48:35 +0100

    connection with database fixed

--- src/main/java/com/marcind/database/DemoClass.java
+++ src/main/java/com/marcind/database/DemoClass.java
@@ -6,12 +6,13 @@ public class DemoClass
 {
 	public static void main(String[] args) throws Exception
 	{
+		Pass passw = new Pass();
 		String url = "jdbc:mysql://localhost:3306/aliens";
 		String uname = "root";
-		String pass = "";
+		String pass = passw.p;
 		String query = "select username from student where userid = 3";
 		
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection(url,uname,pass);
 		Statement st = con.createStatement();
 		ResultSet rs = st.executeQuery(query);
